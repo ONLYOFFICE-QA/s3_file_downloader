38076a27bc30245b3221ddb1655de3c32a01aeb7
l02 <isrLamb@outlook.com>
2021-08-11 10:15:47 +0300

    prod

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+source 'https://rubygems.org'
+gem 'onlyoffice_s3_wrapper', '0.3.0'
+gem 'ooxml_parser'
+gem 'webrick'
---
+++ main.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'onlyoffice_s3_wrapper'
+
+def s3
+  @s3 ||= OnlyofficeS3Wrapper::AmazonS3Wrapper.new(bucket_name: 'conversion-testing-files', region: 'us-east-1')
+end
+
+files = s3.get_files_by_prefix('doc/')
+@tmp_dir = './tmp'
+
+files.each do |el|
+  print(el)
+  s3.download_file_by_name(el, @tmp_dir)
+end
