a82159cbe7a0b3cedea8fae78f654eeca9dc4058
l02 <isrLamb@outlook.com>
2021-08-11 14:27:46 +0300

    add read doc

--- main.rb
+++ main.rb
@@ -2,16 +2,30 @@
 
 require 'onlyoffice_s3_wrapper'
 
+filename_from_txt = []
+filename = s3.get_files_by_prefix('doc/')
+FileUtils.makedirs('./tmp')
+@tmp_dir = './tmp'
+
 def s3
   @s3 ||= OnlyofficeS3Wrapper::AmazonS3Wrapper.new(bucket_name: 'conversion-testing-files', region: 'us-east-1')
 end
 
-files = s3.get_files_by_prefix('doc/')
-FileUtils.makedirs('./tmp')
-@tmp_dir = './tmp'
-
-files.each do |el|
-  p("Starting downloading file: #{el}")
-  # print(el)
-  s3.download_file_by_name(el, @tmp_dir)
+def download(lib)
+  lib.each do |el|
+    el.sub!(/['\n']?$/, '')
+    p("Starting downloading file: #{el}")
+    # print(el)
+    s3.download_file_by_name(el, @tmp_dir)
+  end
 end
+
+
+File.open("text.txt", "r") do |file|
+  file.readlines().each do |line|
+    filename_from_txt.push(line)
+  end
+end
+
+download(filename)
+
